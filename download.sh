1e79b89b25e70eb64ce105355225d2f0136b216d
Amal Prakash <44752430+amalp12@users.noreply.github.com>
2023-08-03 14:13:25 +0530

    Create download.sh

---
+++ download.sh
@@ -0,0 +1,51 @@
+#!/bin/sh
+
+download() {
+    component="$1"
+    echo "Downloading $component .. "
+    wget "https://github.com/eXpOSNitc/$component/archive/master.zip" -c -q -O "$component.zip"
+    unzip -q "$component".zip
+    mv "$component"-master "$component"
+    rm "$component".zip
+    echo "$component downloaded."
+}
+
+myexpos() {
+    download expl
+    download nespl
+    download nexfs-interface
+    download nexsm
+}
+
+bootstrap() {
+    if [ -e mynexpos ]; then
+        echo "mynexpos folder exists."
+        echo "Please remove it and re-run the script to download again."
+        return
+    fi;
+
+    wget -q https://github.com/eXpOSNitc/nexpos-bootstrap/archive/main.zip
+    unzip -q main.zip
+    rm main.zip
+    mv nexpos-bootstrap-main myexpos
+    cd mynexpos
+    ./download.sh
+    rm Readme.md # Because the Readme is for the repo and will confuse if placed
+                 # in myexpos folder
+    echo "Download complete."
+    cd ..
+}
+
+
+if [ -f "Makefile" ]; then
+    # If Makefile is there, continue downloading myexpos components
+    myexpos
+else
+    # The user is executing directly from `wget | sh`
+    # so create the folder and download makefile
+    bootstrap
+
+    # PS: We could have just downloaded the Makefile alone in the bootstrap
+    # function. But downloading the repo provides us the possibilty to provide
+    # additional helper files in the future.
+fi
